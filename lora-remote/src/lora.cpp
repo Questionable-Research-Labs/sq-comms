4496690371f51ba78a22b2e91081204c7acaf829
Jasper Miller-Waugh <contact@jmw.nz>
2023-07-09 22:29:49 +1200

    Memory fixes

--- lora-remote/src/lora.cpp
+++ lora-remote/src/lora.cpp
@@ -10,7 +10,7 @@ void sendRawMessage(char* outgoing) {
     LoRa.endPacket();	   // finish packet and send it
 }
 
-void sendMessage(char* topic, char* payload) {
+void sendMessage(const char* topic, const char* payload) {
     char outgoing[JSON_SERIALISATION_LIMIT];
     sprintf(outgoing, "%s:%s", topic, payload);
     sendRawMessage(outgoing);
@@ -20,12 +20,10 @@ void onReceive(int packetSize) {
     if (packetSize == 0) return;  // if there's no packet, return
 
     // read packet header bytes:
-    char incoming[1024] = "";
+    String incoming = "";
 
-    int index = 0;
     while (LoRa.available()) {
-        incoming[index] = (char)LoRa.read();
-        index++;
+        incoming += (char)LoRa.read();
     }
 
     Serial.println("Message: " + String(incoming));
@@ -36,25 +34,30 @@ void onReceive(int packetSize) {
     Heltec.display->drawString(0, 10, incoming);
     Heltec.display->display();
 
-    const char *ptr = strchr(incoming, ':');
+
+    const char* loraMessage = incoming.c_str();
+
+    const char *ptr = strchr(loraMessage, ':');
     if (!ptr) {
         Serial.println("No topic found in message");
         return;
     }
-    int messageSplitIndex = ptr - incoming;
+    int messageSplitIndex = ptr - loraMessage;
 
     Serial.println("Topic split: " + String(messageSplitIndex));
+    
     // Get the topic and data out of the incoming packet
-    char topic[messageSplitIndex + 2] = {};
+    char topic[messageSplitIndex + 2] = "";
     memcpy(topic, &incoming[0], messageSplitIndex);
     topic[messageSplitIndex + 1] = '\0';
     Serial.println(topic);
+
     // Rest of the message is incoming
-    char data[1024] = {};
-    memcpy(data, &incoming[messageSplitIndex + 1], strlen(incoming) - messageSplitIndex);
+    char data[1024] = "";
+    memcpy(data, &incoming[messageSplitIndex + 1], strlen(loraMessage) - messageSplitIndex);
     Serial.println(data);
 
-    for (int i; i<20; i++) {
+    for (int i = 0; i<20; i++) {
         Serial.print(incoming[i], HEX);
     }
 
