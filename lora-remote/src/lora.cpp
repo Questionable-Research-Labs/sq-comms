47d2fd190067fd37410fd4dec47ff86a9a770f4f
Jasper Miller-Waugh <contact@jmw.nz>
2023-07-10 21:53:42 +1200

    Auto relay code

--- lora-remote/src/lora.cpp
+++ lora-remote/src/lora.cpp
@@ -5,6 +5,11 @@ typedef struct {
     int hopCount;
 } topicPriority;
 
+const size_t handled_packet_max_size = 32;
+const size_t handled_packet_hash_size = 32;
+u32_t handled_packet[handled_packet_max_size] = {0};
+size_t handled_packet_head = 0;             // size_t is an unsigned long
+
 void setupLora() {
     LoRa.setSyncWord(0xe4);  // ranges from 0-0xFF, default 0x34, see API docs
     LoRa.setCodingRate4(8);
@@ -20,13 +25,23 @@ void sendMessage(char* outgoing) {
     LoRa.endPacket();	   // finish packet and send it
 }
 
-void sendMessage(char* topic, char* hops, char* payload) {
+void sendMessage(char* topic, char* hops, char* payload, u32_t packetID) {
     char outgoing[JSON_SERIALISATION_LIMIT];
-    sprintf(outgoing, "%s[;]%s[;]%s", topic, hops, payload);
+
+    while (packetID == 0) {
+        packetID = esp_random();
+    }
+    handled_packet[handled_packet_head] = packetID;
+    handled_packet_head++;
+    if (handled_packet_head >= handled_packet_max_size) {
+        handled_packet_head = 0;
+    }
+
+    sprintf(outgoing, "%u[;]%s[;]%s[;]%s", packetID, topic, hops, payload);
     sendMessage(outgoing);
 }
 void sendMessage(char* topic, char* payload) {
-    sendMessage(topic, "[]", payload);
+    sendMessage(topic, "[]", payload, 0);
 }
 
 void onReceive(int packetSize) {
@@ -93,33 +108,61 @@ int scanForBreak(int* results, const char* src, int srcLength, const char* break
     return resultIndex;
 }
 
+int getMaxHopCount(char* topic) {
+    int maxHopCount = -1;
+    for (int i = 0; i < sizeof(msgTypeMap) / sizeof(topicPriority); i++) {
+        if (strcmp(topic, msgTypeMap[i].topic) == 0) {
+            maxHopCount = msgTypeMap[i].hopCount;
+            break;
+        }
+    }
+    return maxHopCount;
+}
+
 void parsePacket(const char* loraMessage) {
     Serial.println("Parsing packet...");
     // Scan for break points
     int messageSplitIndex[32];
     int messageSplitsCount = scanForBreak(messageSplitIndex, loraMessage, strlen(loraMessage), "[;]", 32);
-    if (messageSplitsCount < 2) {
+    if (messageSplitsCount < 3) {
         Serial.println("Invalid message format!");
         return;
     }
 
+    Serial.println("PacketID split: " + String(messageSplitIndex[0]));
 
-    Serial.println("Topic split: " + String(messageSplitIndex[0]));
+    // Get the packet ID out of the incoming packet
+    char* packetID = copyCharRange(loraMessage, 0, messageSplitIndex[0]);
+    u32_t packetIDInt = strtoul(packetID, NULL, 10);
+
+    // Is the packet already in the handled pile?
+    for (int i = 0; i < handled_packet_max_size; i++) {
+        if (handled_packet[i] == packetIDInt) {
+            Serial.println("Packet already handled!");
+            return;
+        }
+    }
+
+    // Add it to the handled pile
+    handled_packet[handled_packet_head] = packetIDInt;
+    handled_packet_head++;
+    if (handled_packet_head >= handled_packet_max_size) {
+        handled_packet_head = 0;
+    }
 
     // Get the topic and data out of the incoming packet
-    char* topic = copyCharRange(loraMessage, 0, messageSplitIndex[0]);
+    char* topic = copyCharRange(loraMessage, messageSplitIndex[0]+3, messageSplitIndex[1]);
 
     // Get the hops data out of the incoming packet
-    char* hopsData = copyCharRange(loraMessage, messageSplitIndex[0]+3, messageSplitIndex[1]);
+    char* hopsData = copyCharRange(loraMessage, messageSplitIndex[1]+3, messageSplitIndex[2]);
 
     // Rest of the message is incoming
-    char* data = copyCharRange(loraMessage, messageSplitIndex[1]+3, strlen(loraMessage));
+    char* data = copyCharRange(loraMessage, messageSplitIndex[2]+3, strlen(loraMessage));
+
 
     Serial.println(topic);
     Serial.println(hopsData);
     Serial.println(data);
-
-
     
 
 #if defined(HAB_SYSTEM)
@@ -134,7 +177,44 @@ void parsePacket(const char* loraMessage) {
     forwardLED(topic, data);
 #endif
 
+    // Forward packet if needed
+    relayPacket(topic, hopsData, data, packetIDInt);
+
+    // Cleanup
     delete[] topic;
     delete[] data;
+    delete[] hopsData;
+}
 
+void relayPacket(char* topic, char* hopsData, char* data, u32_t packetID) {
+    // Parse the json hops data
+    DynamicJsonDocument doc(512);
+    DeserializationError error = deserializeJson(doc, hopsData);
+    if (error) {
+        Serial.println("Failed to parse hops data");
+        return;
+    }
+    // Is in the following format:
+    // [{from: "chipID", rssi: "56.2", order: "0"}, {from: "chipID2", rssi: "5.2", order: "1"}]
+    int hopsCount = doc.size();
+    int maxHopCount = getMaxHopCount(topic);
+    if (maxHopCount == -1) {
+        Serial.println("Invalid topic!");
+        return;
+    }
+    if (hopsCount + 1 >= maxHopCount) {
+        Serial.println("Max hop count reached! No need to repeat");
+        return;
+    }
+
+    // Add this chip to the hops data
+    doc.createNestedObject();
+    doc[hopsCount]["from"] = chipID;
+    doc[hopsCount]["rssi"] = LoRa.packetRssi();
+    doc[hopsCount]["order"] = hopsCount;
+
+    // Serialise and relay packet
+    char serialisedJSON[512];
+    serializeJson(doc, serialisedJSON, 512);
+    sendMessage(topic, serialisedJSON, data, packetID);
 }
--- lora-remote/src/lora.h
+++ lora-remote/src/lora.h
@@ -16,6 +16,7 @@ void setupLora();
 void onReceive(int packetSize);
 void sendMessage(char* outgoing);
 void sendMessage(char* topic, char* payload);
-void sendMessage(char* topic, char* hops, char* payload);
+void sendMessage(char* topic, char* hops, char* payload, u32_t packetID);
 void sendPing();
 void parsePacket(const char* loraMessage);
+void relayPacket(char* topic, char* hopsData, char* data, u32_t packetID);
